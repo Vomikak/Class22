c25568cbbf8f7a80ce38975d590799d72a8a2f73
Vomikak <71966394+Vomikak@users.noreply.github.com>
2020-09-30 16:49:15 +0530

    Add files via upload

---
+++ bounceOffAlgorithm-master/sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+
+
+
+var ball, ground, canvas, engine, world;
+
+function setup() {
+   canvas = createCanvas(400,400);
+   
+  engine = Engine.create();
+  world = engine.world;
+  var g= {
+  isStatic : true
+
+  }
+  
+  ground = Bodies.rectangle(200,380,400,20,g);
+  World.add (world, ground)
+  var b = {
+  restitution :1.0
+
+  }
+  ball = Bodies.circle(200,200,20, b);
+  World.add(world,ball)
+
+}
+
+function draw() {
+  background(0,0,0);  
+  Engine.update (engine); 
+  rectMode(CENTER);
+  rect (ground.position.x, ground.position.y,400,20);
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x, ball.position.y, 20,20)
+  drawSprites();
+}
